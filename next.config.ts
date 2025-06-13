986d03ed429d14014d9eb01ecb125532fd04136b
Pedro Feiteira <pedrofeiteira.dev@gmail.com>
2025-06-13 17:22:19 +0100

    fix: fix gh actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  basePath: '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
